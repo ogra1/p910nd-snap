5e196149d883a971a996f6fd3d767f6f2f54e413
Oliver Grawert <ogra@ubuntu.com>
2020-06-28 13:10:11 +0000

    initial commit

---
+++ snap/local/p910nd.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+set -x
+
+DEVICE=""
+ADDR=""
+BIDI=""
+
+if [ -n "$(snapctl get device)" ]; then
+	DEVICE="-f $(snapctl get device)"
+fi
+
+if [ -n "$(snapctl get bindaddr)" ]; then
+	ADDR="-i $(snapctl get bindaddr)"
+fi
+
+if [ -n "$(snapctl get bidirectional)" ]; then
+	BIDI="-b"
+fi
+
+exec p910nd -d $DEVICE $ADDR $BIDI
